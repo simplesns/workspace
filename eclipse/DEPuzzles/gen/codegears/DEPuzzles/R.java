02f828b005c52fb9ae9e2bda25116a63ca3d169d
Chetchaiyan <Chetchaiyan@hotmail.com>
2011-01-19 13:08:15 +0700

    1

--- eclipse/DEPuzzles/gen/codegears/DEPuzzles/R.java
+++ eclipse/DEPuzzles/gen/codegears/DEPuzzles/R.java
@@ -77,6 +77,7 @@ public final class R {
         public static final int mainmenu=0x7f030001;
         public static final int options=0x7f030002;
         public static final int puzzleselect=0x7f030003;
+        public static final int ui_puzzleselectlistitem=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
